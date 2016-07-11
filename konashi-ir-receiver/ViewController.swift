c13fe0424caf75a7a7ef0ae9e66390caa8ce729c
Kunihiko Ito <kuni.110.92@gmail.com>
2016-07-12 02:46:56 +0900

    Add find button

--- konashi-ir-receiver/ViewController.swift
+++ konashi-ir-receiver/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var button: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -21,5 +22,7 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func findKonashi(sender: UIButton) {
+    }
 }
 
