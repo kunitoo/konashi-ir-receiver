bbae361f57a89e9cb343e3f61498eb3fe0c9d392
Kunihiko Ito <kuni.110.92@gmail.com>
2016-07-12 01:46:02 +0900

    Initial Commit

---
+++ konashi-ir-receiver/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  konashi-ir-receiver
+//
+//  Created by ItoKunihiko on 2016/07/12.
+//  Copyright © 2016年 ItoKunihiko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
